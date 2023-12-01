1de772bc9c5b4ca75b7929e1abce5554aaf7e2e0
Miles <macamp0328@gmail.com>
2023-12-01 00:04:56 +0000

    chore: progress!

--- util_scripts/consolidate_repo.sh
+++ util_scripts/consolidate_repo.sh
@@ -12,10 +12,14 @@ echo $DIR
 
 util_scripts/.consolidateignore
 
-# Read ignore patterns if .consolidateignore exists
+# Read ignore patterns if .consolidateignore exists and disregard comments and blank lines
 IGNORE_PATTERNS=()
 if [ -f "$DIR/.consolidateignore" ]; then
   while IFS= read -r line; do
+    # Skip comments and blank lines
+    if [[ $line =~ ^[[:space:]]*# || -z $line ]]; then
+      continue
+    fi
     IGNORE_PATTERNS+=("$line")
   done < "$DIR/.consolidateignore"
 fi
@@ -34,9 +38,12 @@ should_ignore() {
     if [[ ${pattern: -1} == "/" ]]; then
       pattern=${pattern%/}
     fi
-    # Check if file path starts with directory pattern or ends with file type pattern
-    if [[ $1 == "$pattern"* || $1 == *"$pattern" ]]; then
+    # Check if file path contains directory pattern or ends with file type pattern
+    if [[ $1 == *"$pattern"* || $1 == *"$pattern" ]]; then
+      echo "File $1 matches pattern $pattern"
       return 0
+    else
+      echo "File $1 does not match pattern $pattern"
     fi
   done
   return 1
@@ -44,10 +51,10 @@ should_ignore() {
 
 # Loop through all files, check if they should be ignored, and concatenate
 find . -type f -not -path '*/\.*' -not -name "$OUTPUT_FILE" -print0 | while IFS= read -r -d $'\0' file; do
-    if ! should_ignore "$file"; then
-        # Include only the first 100 lines of each file
-        echo "===== $file =====" >> "$OUTPUT_FILE"
-        head -n 500 "$file" >> "$OUTPUT_FILE"
-        echo "" >> "$OUTPUT_FILE"
-    fi
+  if ! should_ignore "$file"; then
+    # Include only the first 100 lines of each file
+    echo "===== $file =====" >> "$OUTPUT_FILE"
+    head -n 100 "$file" >> "$OUTPUT_FILE"
+    echo "" >> "$OUTPUT_FILE"
+  fi
 done
