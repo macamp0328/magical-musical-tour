5e33a93939a513383ce7c4612a384e68b17f4732
Miles <macamp0328@gmail.com>
2023-12-01 00:55:08 +0000

    chore: clean up

--- util_scripts/consolidate_repo.sh
+++ util_scripts/consolidate_repo.sh
@@ -4,13 +4,9 @@
 OUTPUT_FILE="repo_contents.txt"
 echo "" > "$OUTPUT_FILE"
 
-echo $PATH
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-echo $DIR
-
-util_scripts/.consolidateignore
+# util_scripts/.consolidateignore
 
 # Read ignore patterns if .consolidateignore exists and disregard comments and blank lines
 IGNORE_PATTERNS=()
@@ -40,10 +36,7 @@ should_ignore() {
     fi
     # Check if file path contains directory pattern or ends with file type pattern
     if [[ $1 == *"$pattern"* || $1 == *"$pattern" ]]; then
-      echo "File $1 matches pattern $pattern"
       return 0
-    else
-      echo "File $1 does not match pattern $pattern"
     fi
   done
   return 1
@@ -52,9 +45,11 @@ should_ignore() {
 # Loop through all files, check if they should be ignored, and concatenate
 find . -type f -not -path '*/\.*' -not -name "$OUTPUT_FILE" -print0 | while IFS= read -r -d $'\0' file; do
   if ! should_ignore "$file"; then
-    # Include only the first 100 lines of each file
-    echo "===== $file =====" >> "$OUTPUT_FILE"
+    echo -e "\n\n===== START OF $file =====\n" >> "$OUTPUT_FILE"
+    echo -e "File: $file" >> "$OUTPUT_FILE"
+    echo -e "Size: $(du -h "$file" | cut -f1)" >> "$OUTPUT_FILE"
+    echo -e "Last Modified: $(date -r "$file")\n" >> "$OUTPUT_FILE"
     head -n 100 "$file" >> "$OUTPUT_FILE"
-    echo "" >> "$OUTPUT_FILE"
+    echo -e "\n===== END OF $file =====\n" >> "$OUTPUT_FILE"
   fi
 done
